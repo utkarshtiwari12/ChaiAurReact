541caf373323f34d38a730ea374ba1a186d08cab
Utkarsh Tiwari <utkarshthor12@gmail.com>
2024-07-31 22:58:35 +0530

    redux Toolkit project

---
+++ RTKTodo/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <Todo/>
+    </>
+  )
+}
+
+export default App
